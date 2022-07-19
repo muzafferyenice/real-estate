1c12fa57adacffb00f49cebc587753a2b5ae40b3
Yunus Pektas <yunuspektas@protonmail.com>
2022-07-19 17:21:04 +0300

    application.yml

--- src/main/java/com/realestate/RealEstateApplication.java
+++ src/main/java/com/realestate/RealEstateApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RealEstateApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(RealEstateApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(RealEstateApplication.class, args);
+	}
 
 }
