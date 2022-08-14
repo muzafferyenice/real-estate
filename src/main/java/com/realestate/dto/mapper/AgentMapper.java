09b2b3a3b79a471ed788bc19f049ffd39e0e29ee
muzafferyenice <muzafferaydin.techproed@gmail.com>
2022-08-14 03:03:58 +0300

    end

--- src/main/java/com/realestate/dto/mapper/AgentMapper.java
+++ src/main/java/com/realestate/dto/mapper/AgentMapper.java
@@ -5,6 +5,8 @@ import com.realestate.domain.Agent;
 import com.realestate.dto.AgentDTO;
 import org.mapstruct.Mapper;
 
+import java.util.List;
+
 @Mapper(componentModel="spring")
 public interface AgentMapper {
 
@@ -12,5 +14,6 @@ public interface AgentMapper {
     Agent agentDTOToAgent(AgentDTO agentDTO);
 
     AgentDTO agentToAgentDTO(Agent agent);
+    List<AgentDTO> map(List<Agent> agents);
 
 }
